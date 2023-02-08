42daeab385bc8f21fa29c555850ebdef72e3f7c0
Shreya <shreya.sati@kiwitech.com>
2023-02-08 15:07:01 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { all } from "axios";
 import React from "react";
 import "./App.css";
 
@@ -6,7 +5,8 @@ const App = () => {
     const [studData, setStudData] = React.useState([]);
     const [presentPageData, setPresentPageData] = React.useState([]);
     const [presentPageVal, setPresentPageVal] = React.useState(1);
-    const [filterParam, setFilterParam] = React.useState(["All"]);
+    const [filteredData, setFilteredData] = React.useState([]);
+
     const getData = async () => {
         let dat = await fetch(
             "https://63e0bacd59bb472a74278f0f.mockapi.io/api/v1/students"
@@ -17,43 +17,57 @@ const App = () => {
     };
 
     const sortData = () => {
-        let sortedDat = studData.sort((a, b) => a.class - b.class);
-        setStudData(sortedDat);
+        let sortedDat = filteredData.sort((a, b) => a.class - b.class);
+        setFilteredData(sortedDat);
         setPresentPageData(
             sortedDat.slice(presentPageVal * 5 - 5, presentPageVal * 5)
         );
     };
 
-    const onPageChange = (pageVal) => {
-        setPresentPageVal(pageVal);
-        setPresentPageData(studData.slice(pageVal * 5 - 5, pageVal * 5));
+    const filterData = (e) => {
+        const option = e.target.value;
+        if (option === "All") {
+            setFilteredData(studData);
+            setPresentPageVal(1);
+            setPresentPageData(studData.slice(0, 5));
+        } else {
+            let filtDat = studData.filter((d) => d.class === option);
+            setFilteredData(filtDat);
+            setPresentPageVal(1);
+            setPresentPageData(filtDat.slice(0, 5));
+        }
     };
 
+    const onPageChange = (pageVal) => {
+        setPresentPageVal(pageVal);
+        setPresentPageData(filteredData.slice(pageVal * 5 - 5, pageVal * 5));
+    };
+    
     React.useEffect(() => {
         getData();
     }, []);
     return (
         <div className="val-table">
+            
+            <h2>Student Data</h2>
+           
+
+            <div className="A">
+           
             <button onClick={sortData}>Sort by Class</button>
-            <h2> Student Data</h2>
-            <select
-    /*
-    // here we create a basic select input
-    // we set the value to the selected value
-    // and update the setFilterParam() state every time onChange is called
-    */
-      onChange={(e) => {
-      setFilterParam(e.target.value);
-       }}
-       className="custom-select"
-       aria-label="Filter Student by Class">
-        <option value="All">Filter By class</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        </select>
+            <label htmlFor="class">Filter by class:</label>
+            <select 
+                name="class" 
+                id="class" 
+                onChange={filterData}>
+                <option value="All">All</option>
+                {[...Array(9)].map((val, ind) => (
+                    <option key={ind} value={ind + 1}>
+                        {ind + 1}
+                </option>
+                ))}
+            </select>
+            </div>
             <table>
                 <thead>
                     <tr>
@@ -84,19 +98,21 @@ const App = () => {
                 </tbody>
             </table>
             <div>
-                {[...Array(Math.ceil(studData.length / 5))].map((val, ind) => (
-                    <button
-                        className={
-                            ind + 1 == presentPageVal
-                                ? "pagination-button selected"
-                                : "pagination-button"
-                        }
-                        key={ind}
-                        onClick={() => onPageChange(ind + 1)}
-                    >
-                        {ind + 1}
-                    </button>
-                ))}
+                {[...Array(Math.ceil(filteredData.length / 5))].map(
+                    (val, ind) => (
+                        <button
+                            className= {`pagination-button ${ind + 1 == presentPageVal ?'selected':''}`} 
+                                // ind + 1 == presentPageVal
+                                //     ? "pagination-button selected"
+                                //     : "pagination-button"
+                            // }
+                            key={ind}
+                            onClick={() => onPageChange(ind + 1)}
+                        >
+                            {ind + 1}
+                        </button>
+                    )
+                )}
             </div>
         </div>
     );
